d90313c31513d6882b2eb788bd6e0fff117d44de
Lucas Caetano <lucas.caetanov2013@gmail.com>
2021-06-14 20:15:43 -0300

    Update t1.hs

--- t1.hs
+++ t1.hs
@@ -17,8 +17,8 @@ lin = 9
 
 -----------------------------------------------------------------
 ---------------------------------------------------------------
-pallet :: Int -> Int -> [(Int,Int,Int)]
-pallet n color= 
+palette :: Int -> Int -> [(Int,Int,Int)]
+palette n color= 
   if color == 1  then [(70+i*10,0,0) | i <- [0..(n-1)] ]
   else if color == 2 then [(0,70+i*10,0) | i <- [0..(n-1)] ] 
   else [(0,0,70+i*10) | i <- [0..(n-1)] ]
@@ -46,8 +46,8 @@ svgStyle (r,g,b) = printf "fill:rgb(%d,%d,%d); mix-blend-mode: screen;" r g b
 -------------------------------------------------------------------------------
 -------------------------------------------------------------------------------
 
-genRectsInLine :: Int -> Int -> [Rect]
-genRectsInLine n c =
+rectAlinhar :: Int -> Int -> [Rect]
+rectAlinhar n c =
   [((m*(w+gap),(fromIntegral c)*(w+(2*gap))),w,w) | m <- [0..fromIntegral (n-1)]]
     where 
          gap = 
@@ -59,22 +59,22 @@ genRectsInLine n c =
           else if odd(n) && even(c) then 200
           else 300 
 -----------------------------------         
-genLineOfRects :: Int -> Int -> [String]
-genLineOfRects lin col = 
-  ["  " ++ svgRect (last (genRectsInLine l c)) (svgStyle (last (pallet(l+(color*c)) color)))  | l <- [1..lin], c <- [col]]
+linhasRect :: Int -> Int -> [String]
+linhasRect lin col = 
+  ["  " ++ svgRect (last (rectAlinhar l c)) (svgStyle (last (palette(l+(color*c)) color)))  | l <- [1..lin], c <- [col]]
     where 
           color =
             if odd(lin) && odd(col) then 1
             else if odd(lin) && even(col)  then 2
             else 3
 -------------------------
-genColumnsOfRects :: Int -> Int -> [[String]]
-genColumnsOfRects lin col =  
-  [genLineOfRects l c | l <- [lin], c <- [0..(col-1)]]
+colunasRect :: Int -> Int -> [[String]]
+colunasRect lin col =  
+  [linhasRect l c | l <- [lin], c <- [0..(col-1)]]
 
 printRects :: Int -> Int -> String
 printRects l c = 
-  printf (unlines $ (concat (genColumnsOfRects l c)))
+  printf (unlines $ (concat (colunasRect l c)))
 
 -------------------------------------------------------------------------------
 
